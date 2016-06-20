f94c188ff6b78e3f03a4207515945b18501d3e3c
mbehbehani <mbehbehani@teladoc.com>
2016-06-20 18:33:21 -0500

    Adds specs for DataGenerator

--- src/test/scala/DataGeneratorSpec.scala
+++ src/test/scala/DataGeneratorSpec.scala
@@ -1,10 +1,22 @@
 import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.Matchers._
 
-class DataGeneratorSpec extends FunSpec {
-  val ec = scala.concurrent.ExecutionContext.Implicits.global
 
-  describe("A random address") {
-    val testJson = DataGenerator.generateJson(3)
-    println(testJson)
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class DataGeneratorSpec extends FunSpec with ScalaFutures {
+  describe("Generated JSON") {
+    val testJson = DataGenerator.generateJson(3).futureValue
+    describe("the first item in the return tuple") {
+      it("matches the id passed into the generateJson function") {
+        assert(testJson._1 == 3)
+      }
+    }
+    describe("the second item in the return tuple") {
+      it("matches the template") {
+        testJson._2 should include("first_name")
+      }
+    }
   }
 }
