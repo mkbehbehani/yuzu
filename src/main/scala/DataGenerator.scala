b9355350af6173abbea916bb25ab26c40ef76bfb
mbehbehani <mbehbehani@teladoc.com>
2016-06-20 18:03:21 -0500

    Adds data generator

---
+++ src/main/scala/DataGenerator.scala
@@ -0,0 +1,29 @@
+import yuzu.RandomData
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object DataGenerator {
+  def generateJson(id:Int)(implicit ec: ExecutionContext): Future[(Int,String)] = Future(id,{
+    val first_name = RandomData.firstName
+    val last_name = RandomData.lastName
+    val streetAddress = RandomData.address
+    val city = RandomData.city
+    val state = RandomData.state
+    val zipCode = RandomData.zipCode
+    val phone = RandomData.phone
+    val username = RandomData.username(first_name, last_name)
+    val email = RandomData.email(first_name, last_name)
+    s""" {
+           "id": "$id",
+           "primary_name": "$first_name $last_name",
+           "first_name": "$first_name",
+           "last_name": "$last_name",
+           "address": "$streetAddress",
+           "city": "$city",
+           "state": "$state",
+           "zip_code": "$zipCode",
+           "phone": "$phone",
+           "email": "$email",
+           "username": "$username"}"""}
+  )
+}
