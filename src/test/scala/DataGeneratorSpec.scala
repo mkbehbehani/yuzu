35ddb292ecd9cbedddd6069f4fe9cbe1827ca901
mbehbehani <mbehbehani@teladoc.com>
2016-06-20 18:50:08 -0500

    Updates DataGeneratorSpec setup

--- src/test/scala/DataGeneratorSpec.scala
+++ src/test/scala/DataGeneratorSpec.scala
@@ -6,14 +6,15 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class DataGeneratorSpec extends FunSpec with ScalaFutures {
   describe("Generated JSON") {
-    val testJson = DataGenerator.generateJson(3).futureValue
     describe("the first item in the return tuple") {
       it("matches the id passed into the generateJson function") {
+        val testJson = DataGenerator.generateJson(3).futureValue
         assert(testJson._1 == 3)
       }
     }
     describe("the second item in the return tuple") {
       it("matches the template") {
+        val testJson = DataGenerator.generateJson(3).futureValue
         testJson._2 should include("first_name")
       }
     }
